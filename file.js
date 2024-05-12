8ba8ab3b063d8476ab0819abd79d84347385c188
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-05-13 01:07:05 +0200

    count to 30

--- file.js
+++ file.js
@@ -1,10 +1,11 @@
 console.log('Programmstart');
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 30; i++) {
     console.log(i);
 }
 
 function hello() {
     console.log("Hello World!");
 }
+
 
